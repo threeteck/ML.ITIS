f54cf4f10aee68f20afa33e4daf377b2eff7dafb
threeteck <threeteck@gmail.com>
2022-12-20 01:01:07 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+﻿# Face detection in real-time with webcam using OpenCV and Haar Cascades
+
+import numpy as np
+import cv2
+
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+def preprocess(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.equalizeHist(img)
+    return img
+
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    return frame
+
+
+video_capture = cv2.VideoCapture(0)
+while True:
+    _, frame = video_capture.read()
+    preprocessed = preprocess(frame)
+    detected = detect(preprocessed, frame)
+    cv2.imshow('Video', detected)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
